143d623d34efa17860f5edfe11e5996d30a01da1
Aashish Sharma <aashish1502@gmail.com>
2017-04-03 17:05:44 +0530

    navbar changes

--- public/component/navbar.jsx
+++ public/component/navbar.jsx
@@ -2,6 +2,11 @@ var React = require('react');
 var {Link,IndexLink} = require('react-router');
 
 var navbar = React.createClass({
+	onSearch :function(e){
+		e.preventDefault();
+		alert("not implemented yet");
+
+	},
 	render:function(){
 		return(
 			<div className="top-bar">
@@ -11,6 +16,12 @@ var navbar = React.createClass({
 							<li><IndexLink to="/" activeClassName= "active" activeStyle={{fontWeight:'bold'}}> Get Weather</IndexLink></li>
 							<li><Link to="/about" activeClassName= "active" activeStyle={{fontWeight:'bold'}}> About </Link></li>
 							<li><Link to ="/examples" activeClassName= "active" activeStyle={{fontWeight:'bold'}}>Examples </Link></li>
+					</ul>
+				</div>
+				<div className="top-bar-right">
+					<ul class="menu">
+						<li><input type="search" placeholder="Location" ref="location"/></li>
+						<li><button type="button" class="button" onClick={this.onSearch}>Get Weather</button></li>
 					</ul>
 				</div>
 			</div>
