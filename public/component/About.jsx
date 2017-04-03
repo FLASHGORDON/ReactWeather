8159f71ac31d39be5c0cb84389ab9423c01fed20
Aashish Sharma <aashish1502@gmail.com>
2017-04-03 21:22:22 +0530

    About page update

--- public/component/About.jsx
+++ public/component/About.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 var About = React.createClass({
 	render:function(){
 		return(
-			<h1>About component</h1>
+			<div>
+				<h1 className="text-center">About</h1>
+				<p>This is a Webapp coded in React to tell the weather of a specified location.</p>
+				<p>ReactWeather uses JSX, Webpack and Foundation framework.</p>
+			</div>
 			);
 	}
 });
