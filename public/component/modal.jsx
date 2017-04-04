f1b978b05d0a845b12159df47fb904793fd5c1c7
Aashish Sharma <aashish1502@gmail.com>
2017-04-04 08:31:45 +0530

    modal added

---
+++ public/component/modal.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var Errmsg = React.createClass({
+    componentDidMount:function(){
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render:function(){
+        return(
+            <div id="error-modal" className="reveal tiny text-center" data-reveal>
+                <h4>Some text</h4>
+                <p>Our message</p>
+                <button className="button hollow" data-close>Okay</button>
+            </div>
+        );
+    }
+});
+
+module.exports= Errmsg;
